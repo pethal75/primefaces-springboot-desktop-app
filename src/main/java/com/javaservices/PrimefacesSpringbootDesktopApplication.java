2422f655fe2d79c2c1417a26e57a5b9a7c56f3bb
pehalada <peter.halada@dhl.com>
2024-05-30 14:06:37 +0200

    Fixing sonarlint warnings

--- src/main/java/com/javaservices/PrimefacesSpringbootDesktopApplication.java
+++ src/main/java/com/javaservices/PrimefacesSpringbootDesktopApplication.java
@@ -1,28 +1,6 @@
 package com.javaservices;
 
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-import com.sun.tools.javac.Main;
 import jakarta.annotation.PostConstruct;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import javax.net.ssl.HttpsURLConnection;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.WebResourceSet;
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.EmptyResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -42,7 +20,7 @@ public class PrimefacesSpringbootDesktopApplication extends SpringBootServletIni
 
     @PostConstruct
     public void showBrowser() {
-        Runnable startJavyFX = () -> {new JavaFXBrowser().openBrowser();};
+        Runnable startJavyFX = JavaFXBrowser::openBrowser;
         new Thread(startJavyFX).start();
     }
 }
